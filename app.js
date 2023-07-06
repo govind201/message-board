99294eb3a21a60a418bbbe4e24097bbe956a4e06
Govind Singh <gs5818475@gmail.com>
2023-07-06 15:29:08 +0530

    revise code format

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
-const cookieParser = require("cookie-parser");
 const createError = require("http-errors");
-
-
 const indexRouter = require("./routes/index");
 
 const app = express();
@@ -15,7 +12,6 @@ app.set("views", path.join(__dirname, "views"));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "assets")));
 
 app.use("/", indexRouter);
@@ -26,8 +22,6 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
   res.status(err.status || 500);
   res.render("error");
 });
