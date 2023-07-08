0c2022408c88f199daadb1793c0cd242a7bfcc66
Govind Singh <gs5818475@gmail.com>
2023-07-08 18:00:16 +0530

    add logger in dev only

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(logger("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger('dev'));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "assets")));
